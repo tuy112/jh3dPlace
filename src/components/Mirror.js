14d7d1c8ecc73350ab6bf0a20ebe7cd055408b8b
Kim Jae Hyeok <master_catch@naver.com>
2024-09-11 15:48:28 +0900

    거울 추가

---
+++ src/components/Mirror.js
@@ -0,0 +1,32 @@
+import { useRef, useEffect } from 'react';
+import * as THREE from 'three';
+import { useThree } from '@react-three/fiber';
+import { Reflector } from 'three/examples/jsm/objects/Reflector';
+
+const Mirror = () => {
+  const { scene } = useThree();
+  const mirrorRef = useRef();
+
+  useEffect(() => {
+    const geometry = new THREE.PlaneGeometry(5, 5);
+    const reflector = new Reflector(geometry, {
+      clipBias: 0.003,
+      textureWidth: window.innerWidth * window.devicePixelRatio,
+      textureHeight: window.innerHeight * window.devicePixelRatio,
+      color: 0x889999,
+    });
+
+    reflector.position.set(0, 1, -5); // 거울 위치
+
+    mirrorRef.current = reflector;
+    scene.add(reflector);
+
+    return () => {
+      scene.remove(reflector);
+    };
+  }, [scene]);
+
+  return null;
+};
+
+export default Mirror;

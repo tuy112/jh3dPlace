75661c6841d6e40c505e736365ba564f62ee02ee
Kim Jae Hyeok <master_catch@naver.com>
2024-09-11 15:48:21 +0900

    바닥면 추가 (+물리엔진)

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { usePlane } from '@react-three/cannon';
 import { PlaneGeometry, MeshStandardMaterial} from 'three';
 
 function Ground() {
+    // 물리적 바닥 설정
+    const [ref] = usePlane(() => ({
+        rotation: [-Math.PI / 2, 0, 0],  // 바닥을 수평으로 회전
+        position: [0, -5, 0],            // 바닥 위치 설정
+      }));
+
     const geometry = new PlaneGeometry(500, 500);
     const material = new MeshStandardMaterial({ color: 'gray' });
 
     return (
         <mesh 
-            position={[0, -5, 0]} 
-            rotation-x={-Math.PI / 2} 
+            ref={ref}
             geometry={geometry} 
             material={material} 
+            receiveShadow
         />
     );
 }
